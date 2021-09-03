91955a9da10f7bdb6dc791d8ef9b1e48a128323b
Phillip Bitzer <pm.bitzer@uah.edu>
2021-09-03 15:42:27 -0500

    Apply further reviewer code and style feedback to state monitor

--- plugins/state_monitor.py
+++ plugins/state_monitor.py
@@ -3,10 +3,12 @@ Plugin to monitor state variables from the charge controller.
 """
 
 # Standard library imports
+import abc  # For the MessageSender ABC
+import logging  # For getting the logger from SlackSender
+import urllib.parse 
+import urllib.request
+from configparser import ConfigParse
 from pathlib import Path
-import urllib.request  # for SlackSender
-import urllib.parse  # for SlackSender
-from configparser import ConfigParser  # for SlackSender
 
 # Local imports
 import brokkr.pipeline.base
@@ -14,25 +16,31 @@ import brokkr.pipeline.decode
 
 
 class StateMonitor(brokkr.pipeline.base.OutputStep):
-    """ Handle notifications for changes in state variables. """
+    """Handle notifications for changes in state variables."""
 
-    def __init__(self, method=None, power_delim=1,
-                 slack_channel=None,
-                 slack_key_file=None,
-                 **output_step_kwargs):
+    def __init__(
+        self,
+        method=None,
+        power_delim=1,
+        slack_channel=None,
+        slack_key_file=None,
+        **output_step_kwargs,
+        ):
         """
+Handle notifications for changes in state variables.
 
         Parameters
         ----------
         method : str
             The method of how we're going to send notifications. If `None`, then
             we'll only log them.
-        power_delim : numeric
-            The delimiter between normal power and low power. If power falls below this
-            value, it is considered low and a notification will be generated.
-        slack_channel : str
+        power_delim : numeric, optional
+            The delimiter between normal power and low power.
+            If power falls below this value, it is considered low
+            and a notification will be generated.
+        slack_channel : str, optional
             If `method=='slack'`, then this is the channel in Slack to post notifications.
-        slack_key_file : str
+        slack_key_file : str, optional
             If `method=='slack'`, then the path to the file that contains the Slack key
         output_step_kwargs : **kwargs, optional
             Keyword arguments to pass to the OutputStep constructor.
@@ -53,10 +61,14 @@ class StateMonitor(brokkr.pipeline.base.OutputStep):
                 self.method_cls = SlackSender(slack_key_file, slack_channel,
                                               logger=self.logger)
                 # todo: pass in logger instance or use logging.getLogger(__name__)?
-            except FileNotFoundError:
-                self.logger.error("File not found error. Is the Slack key file in right place?")
-            except Exception as exp:  # if anything goes wrong, don't set the sender class
-                self.logger.error(f"Unknown error initializing SlackSender: {type(exp)}")
+            except FileNotFoundError as e:
+                self.logger.error(
+                    "%e initializing Slack sender: %s Is the Slack key file in the right place?",
+                    type(e).__name__, e)
+                self.logger.info("Error details:", exc_info=True)
+            except Exception as e:  # if anything goes wrong, don't set the sender class
+                self.logger.error("Unexpected %s initializing SlackSender: %s", type(e).__name__, e)
+                self.logger.info("Error details:", exc_info=True)
 
     def execute(self, input_data=None):
         """
@@ -64,10 +76,10 @@ class StateMonitor(brokkr.pipeline.base.OutputStep):
 
         Parameters
         ----------
-        input_data : any, optional
+        input_data : Mapping[str, DataValue], optional
             Per iteration input data passed to this function from previous
-            PipelineSteps. Not used here but retained for compatibility with
-            the generalized PipelineStep API. The default is None.
+            PipelineSteps. Used to extract the data values to report.
+            The default is None.
 
         Returns
         -------
@@ -92,7 +104,7 @@ class StateMonitor(brokkr.pipeline.base.OutputStep):
             load_now, load_pre = now_then('adc_vl_f')
             curr_now, curr_pre = now_then('adc_il_f')
 
-            power_now, power_pre = [load_now*curr_now, load_pre*curr_pre]
+            power_now, power_pre = load_now*curr_now, load_pre*curr_pre
             if (power_now < self.power_delim) and (power_pre > self.power_delim):
                 msg = f"Power has dropped from {power_pre:.2f} to {power_now:.2f}."
                 self.logger.info(msg)
@@ -101,7 +113,7 @@ class StateMonitor(brokkr.pipeline.base.OutputStep):
             # Sensor communication
             comm_now, comm_pre = now_then('ping')
             # If the ping !=0, then we can't reach the sensor
-            if (comm_now != 0) and (comm_pre == 0):
+            if comm_now and not comm_pre:
                 msg = "No communication with sensor!"
                 self.logger.info(msg)
                 self.send_message(msg)
@@ -164,9 +176,9 @@ class StateMonitor(brokkr.pipeline.base.OutputStep):
         if self.method_cls is not None:
             self.method_cls.send(msg)
 
-    @classmethod
-    def construct_message(cls, msg):
-        """ Construct the message detailing the state notification. """
+    @staticmethod
+    def construct_message(msg):
+        """Construct the message detailing the state notification."""
         from brokkr.config.unit import UNIT_CONFIG
         from brokkr.config.metadata import METADATA
 
@@ -179,25 +191,34 @@ class StateMonitor(brokkr.pipeline.base.OutputStep):
         return msg
 
 
-class SlackSender:
+class MessageSender(metaclass=abc.ABCMeta):
+    """An abstract message sender for the state monitor."""
+    # Methods, attributes and code common to all senders go here.
+    # For example, the logger
+    @abc.abstractmethod
+    def send(self, msg):
+        pass
+
+
+class SlackSender(MessageSender):
     def __init__(self, key_file, channel, logger=None):
         """ 
         Handle sending messages in Slack. 
         
         Parameters
         ----------
-        key_file : str
+        key_file : str or pathlib.Path
             The path to the file that contains the Slack key for the
             workspace to send the message.
         channel : str
             The channel to send the message to. It should be defined in file
             provided by `key_file`.
-        logger : Python logger
-            If not None, if message send fails, log it here.
+        logger : logging.Logger or False, optional
+            If not False and message send fails, log it to the passed logger
+            or the default logger, if not passed.
 
         """
-
-        self.logger = logger
+        self.logger = logging.getLogger(__name__) if logger is None else logger
 
         slack_key_file = Path(key_file).expanduser()
 
@@ -230,7 +251,7 @@ class SlackSender:
         resp = urllib.request.urlopen(self.request)
 
         if resp.status != 200:
-            warning_msg = f"'Sending to slack failed. Message: {msg}"
+            warning_msg = f"Sending to slack failed. Message: {msg}"
 
             if self.logger:
                 self.logger.warning(warning_msg)
