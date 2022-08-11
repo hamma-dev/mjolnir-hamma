e357a535b691d9efd5707f00cd3537413fe1f4dc
Phillip Bitzer <pm.bitzer@uah.edu>
2022-08-11 10:09:07 -0500

    Update cell network

--- files/50_bring_wwan0_up.py
+++ files/50_bring_wwan0_up.py
@@ -12,6 +12,6 @@ time.sleep(60)
 print("Connecting to wwan0...")
 modem_list = subprocess.run(["mmcli", "-L"], check=True, encoding="utf-8", stdout=subprocess.PIPE)
 modem_number = modem_list.stdout.strip().split(" ")[0].split("/")[-1]
-subprocess.run(["mmcli", "-m", modem_number, "--simple-connect=apn=NXTGENPHONE"], check=True)
+subprocess.run(["mmcli", "-m", modem_number, "--simple-connect=apn=h2g2"], check=True)
 print("Connected to wwan0")
 sys.exit()
