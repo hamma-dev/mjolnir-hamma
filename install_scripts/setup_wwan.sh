b974013fd1d8e9d6d8d6db89ac92310fabd2cc8e
Phillip Bitzer <pm.bitzer@uah.edu>
2023-04-24 16:51:39 -0500

    Create setup_wwan.sh - WIP

---
+++ install_scripts/setup_wwan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Set cell modem access (WWAN) 
+
+# Path to the files
+FILES_PATH="/home/pi/dev/mjolnir-hamma/files/"
+
+# Paths to where the files should go
+DEGRADED_PATH="//etc/networkd-dispatcher/degraded.d/"
+NETWORK_PATH="/etc/systemd/network/"
+
+WWAN_UP_FILE="50_bring_wwan0_up.py"
+WWAN_CONFIG_FILE="20-wwan0.network"
+
+# Install needed packages
+apt-get install udhcpc libqmi-utils
+
+cp $FILES_PATH$WWAN_UP_FILE $DEGRADED_PATH
+cp $FILES_PATH$WWAN_CONFIG_FILE $NETWORK_PATH
+
+# Set proper permissions
+chgrp root $DEGRADED_PATH$WWAN_UP_FILE
+chown root $DEGRADED_PATH$WWAN_UP_FILE
+chmod a+x $DEGRADED_PATH$WWAN_UP_FILE
+
+
+# Change some permissions
+#sudo chmod 0755 /etc/systemd/system/wpa_supplicant@wlan0.service.d/
+#sudo chmod 0644 /etc/systemd/system/wpa_supplicant@wlan0.service.d/override.conf
